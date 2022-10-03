609b3a21aff32322e7bdc80cbd8313150936e321
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-03 23:24:37 +0200

    setting up testing for palindrome

--- monkeyTalk.test.js
+++ monkeyTalk.test.js
@@ -1,14 +1,14 @@
-const monkeyTalk = require("./monkeyTalk");
+const monkeyTalk = require('./monkeyTalk');
 
-test("empty string", () => {
-  expect(monkeyTalk("")).toBe("Ook.");
+test('empty string', () => {
+  expect(monkeyTalk('')).toBe('Ook.');
 });
 test('entering "alma" should return "eek"', () => {
-  expect(monkeyTalk("alma")).toBe("Eek.");
+  expect(monkeyTalk('alma')).toBe('Eek.');
 });
 test('entering "Arany János" should return "eek"', () => {
-  expect(monkeyTalk("Arany János")).toBe("Eek ook.");
+  expect(monkeyTalk('Arany János')).toBe('Eek ook.');
 });
 test('entering "Apple," should return "eek"', () => {
-  expect(monkeyTalk("Apple,")).toBe("Eek.");
+  expect(monkeyTalk('Apple,')).toBe('Eek.');
 });
---
+++ palindromeChainLength.test.js
@@ -0,0 +1,24 @@
+import {
+  reverseNumber,
+  isPalindrome,
+  palindromeChainLength,
+} from './palindromeChainLength';
+
+test('entering "" should return ""', () => {
+  expect(reverseNumber('')).toBe('');
+});
+test('entering "" should return ""', () => {
+  expect(isPalindrome('')).toBe('');
+});
+test('takes 0 iterations to get a palindrome', () => {
+  expect(palindromeChainLength('')).toBe('');
+});
+test('takes 1 iterations to get a palindrome', () => {
+  expect(palindromeChainLength('')).toBe('');
+});
+test('takes a large number of iterations to get a palindrome', () => {
+  expect(palindromeChainLength('')).toBe('');
+});
+test('should first', () => {
+  expect('').toBe('');
+});
